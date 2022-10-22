ad9b57d63a228de63688d12d5dd2872f4cfbaec8
Osama <72892044+muhammadosama984@users.noreply.github.com>
2022-10-23 01:21:02 +0500

    almost complete

--- server/index.js
+++ server/index.js
@@ -23,6 +23,10 @@ io.on("connection", (socket) => {
     console.log(`User with ID: ${socket.id} joined room: ${data}`);
   });
 
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("recieve_message", data);
+    console.log(data);
+  });
   socket.on("disconnect", () => {
     console.log("User is disconnected", socket.id);
   });
